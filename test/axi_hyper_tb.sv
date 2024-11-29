4d157b1cd6f17186f8325091423b2efe3e919d1b
Philippe Sauter <phsauter@iis.ee.ethz.ch>
2024-11-29 22:57:21 +0100

    Set Hyperram chips conf register in tb

--- test/axi_hyper_tb.sv
+++ test/axi_hyper_tb.sv
@@ -28,11 +28,13 @@ module axi_hyper_tb
   /// Test time of the DUT
   parameter time         TbTestTime =  4ns
 );
+  import hyperbus_tb_pkg::*;
   /////////////////////////////
   // Axi channel definitions //
   /////////////////////////////
   `include "axi/typedef.svh"
   `include "axi/assign.svh"
+
 
   /////////////////////////
   // Clock and Reset gen //
@@ -59,9 +61,11 @@ module axi_hyper_tb
 
   logic                  end_of_sim;
 
-  ////////////////////////////////
-  // Stimuli generator typedefs //
-  ////////////////////////////////
+
+  ///////////////////////
+  // AXI Random Master //
+  ///////////////////////
+  // AXI master for random data transactions
   typedef axi_test::axi_rand_master #(
     .AW                   ( TbAxiAddrWidthFull ),
     .DW                   ( TbAxiDataWidthFull ),
@@ -92,20 +96,12 @@ module axi_hyper_tb
     .TT( TbTestTime         )
   ) axi_scoreboard_mst_t;
 
-  typedef reg_test::reg_driver #(
-    .AW ( RegBusAW   ),
-    .DW ( RegBusDW   ),
-    .TT ( TbTestTime )
-  ) reg_bus_master_t;   
-
-  logic s_reg_error;
-   
   AXI_BUS_DV #(
     .AXI_ADDR_WIDTH ( TbAxiAddrWidthFull ),
     .AXI_DATA_WIDTH ( TbAxiDataWidthFull ),
     .AXI_ID_WIDTH   ( TbAxiIdWidthFull   ),
     .AXI_USER_WIDTH ( TbAxiUserWidthFull )
-  ) axi_mst_intf_dv (
+  ) axi_rand_intf_dv (
     .clk_i ( clk )
   );
 
@@ -118,12 +114,125 @@ module axi_hyper_tb
     .clk_i ( clk )
   );
 
-  `AXI_ASSIGN_MONITOR(score_mst_intf_dv, axi_mst_intf_dv)
+  AXI_BUS #(
+    .AXI_ADDR_WIDTH ( TbAxiAddrWidthFull ),
+    .AXI_DATA_WIDTH ( TbAxiDataWidthFull ),
+    .AXI_ID_WIDTH   ( TbAxiIdWidthFull   ),
+    .AXI_USER_WIDTH ( TbAxiUserWidthFull )
+  ) axi_rand_intf ();
+
+  `AXI_ASSIGN_MONITOR(score_mst_intf_dv, axi_rand_intf_dv)
+  `AXI_ASSIGN(axi_rand_intf, axi_rand_intf_dv)
+
+
+
+  ////////////////////////
+  // AXI Control Master //
+  ////////////////////////
+
+  AXI_BUS_DV #(
+    .AXI_ADDR_WIDTH ( TbAxiAddrWidthFull ),
+    .AXI_DATA_WIDTH ( TbAxiDataWidthFull ),
+    .AXI_ID_WIDTH   ( TbAxiIdWidthFull   ),
+    .AXI_USER_WIDTH ( TbAxiUserWidthFull )
+  ) axi_ctrl_intf_dv (
+    .clk_i ( clk )
+  );
+
+  AXI_BUS #(
+    .AXI_ADDR_WIDTH ( TbAxiAddrWidthFull ),
+    .AXI_DATA_WIDTH ( TbAxiDataWidthFull ),
+    .AXI_ID_WIDTH   ( TbAxiIdWidthFull   ),
+    .AXI_USER_WIDTH ( TbAxiUserWidthFull )
+  ) axi_ctrl_intf ();
+
+  typedef axi_test::axi_driver #(
+    .AW ( TbAxiAddrWidthFull ),
+    .DW ( TbAxiDataWidthFull ),
+    .IW ( TbAxiIdWidthFull   ),
+    .UW ( TbAxiUserWidthFull ),
+    .TA ( TbApplTime         ),
+    .TT ( TbTestTime         )
+  ) axi_ctrl_master_t;
+  axi_ctrl_master_t axi_ctrl_mst = new( axi_ctrl_intf_dv );
+
+  `AXI_ASSIGN(axi_ctrl_intf, axi_ctrl_intf_dv)
+
+  logic s_axi_error;
+
+
+  //////////////////////////////
+  // AXI Control Master Tasks //
+  //////////////////////////////
+  task automatic axi_write_32(
+    input axi_addr_t  addr,
+    input bit [31:0] data
+  );
+    axi_ctrl_master_t::ax_beat_t ax = new();
+    axi_ctrl_master_t::w_beat_t w = new();
+    axi_ctrl_master_t::b_beat_t b;
+    
+    @(posedge clk);
+    ax.ax_addr  = addr;
+    ax.ax_id    = 0;
+    ax.ax_len   = 0;
+    ax.ax_size  = 2;
+    ax.ax_burst = axi_pkg::BURST_INCR;
+    axi_ctrl_mst.send_aw(ax);
+    w.w_strb = 'h0F;
+    w.w_data = data;
+    w.w_last = 1;
+    axi_ctrl_mst.send_w(w);
+    axi_ctrl_mst.recv_b(b);
+    if (b.b_resp != axi_pkg::RESP_OKAY)
+      $error("[AXI-CTRL] - Write error response: %d!", b.b_resp);
+  endtask
+
+
+  ///////////////////
+  // Regbus Master //
+  ///////////////////
+  typedef reg_test::reg_driver #(
+    .AW ( RegBusAW   ),
+    .DW ( RegBusDW   ),
+    .TT ( TbTestTime )
+  ) reg_bus_master_t;   
+
+  logic s_reg_error;
 
   REG_BUS #(
     .ADDR_WIDTH(RegBusAW),
     .DATA_WIDTH(RegBusDW)
   )  reg_bus_mst (.clk_i (clk));
+
+
+
+  ////////////////////
+  // AXI Master MUX //
+  ////////////////////
+
+  AXI_BUS #(
+    .AXI_ADDR_WIDTH ( TbAxiAddrWidthFull  ),
+    .AXI_DATA_WIDTH ( TbAxiDataWidthFull  ),
+    .AXI_ID_WIDTH   ( TbAxiIdWidthFull +1 ),
+    .AXI_USER_WIDTH ( TbAxiUserWidthFull  )
+  ) axi_dut_intf ();
+
+  axi_mux_intf #(
+    .SLV_AXI_ID_WIDTH ( TbAxiIdWidthFull    ),
+    .MST_AXI_ID_WIDTH ( TbAxiIdWidthFull +1 ),
+    .AXI_ADDR_WIDTH   ( TbAxiAddrWidthFull  ),
+    .AXI_DATA_WIDTH   ( TbAxiDataWidthFull  ),
+    .AXI_USER_WIDTH   ( TbAxiUserWidthFull  ),
+    .NO_SLV_PORTS     ( 2 )
+  ) i_axi_mst_mux (
+      .clk_i  ( clk   ),
+      .rst_ni ( rst_n ),
+      .test_i ( 1'b0  ),
+      .slv    ( { axi_ctrl_intf, axi_rand_intf } ),
+      .mst    ( axi_dut_intf )
+  );
+
    
   ////////////////////
   // Address Ranges //
@@ -136,18 +245,21 @@ module axi_hyper_tb
      
   initial begin : proc_sim_crtl
 
-    automatic axi_scoreboard_mst_t   mst_scoreboard  = new( score_mst_intf_dv );
-    automatic axi_rand_master_t      axi_master      = new( axi_mst_intf_dv   );
-    automatic reg_bus_master_t       reg_master      = new( reg_bus_mst       );
+    automatic axi_scoreboard_mst_t mst_scoreboard = new( score_mst_intf_dv );
+    automatic axi_rand_master_t    axi_rand_mst   = new( axi_rand_intf_dv  );
+    automatic reg_bus_master_t     reg_master     = new( reg_bus_mst       );
+    
+    automatic s27ks_cfg0_reg_t s27ks_cfg0 = hyperbus_tb_pkg::s27ks_cfg0_default;
 
     // Reset the AXI drivers and scoreboards
     end_of_sim = 1'b0;
     mst_scoreboard.reset();
-    axi_master.reset();
+    axi_rand_mst.reset();
+    axi_ctrl_mst.reset_master();
     reg_master.reset_master();
 
     // Set some mem regions for rand axi master
-    axi_master.add_memory_region(32'h8000_0000, 32'h8000_0000 + ( TbDramDataWidth * TbDramLenWidth ), axi_pkg::NORMAL_NONCACHEABLE_BUFFERABLE);
+    axi_rand_mst.add_memory_region(32'h8000_0000, 32'h8000_0000 + ( TbDramDataWidth * TbDramLenWidth ), axi_pkg::NORMAL_NONCACHEABLE_BUFFERABLE);
      
     mst_scoreboard.enable_all_checks();
 
@@ -156,11 +268,24 @@ module axi_hyper_tb
 
     #600350ns;
 
+    // switch memory address space to register space
+    reg_master.send_write(32'h7<<2, 1'b1, '1, s_reg_error);
+    if (s_reg_error != 1'b0) $error("unexpected error");
+
+    // enable variable latency so we can test RWDS sampling
+    s27ks_cfg0.fixed_latency_enable = 1'b0;
+    $display("t3est");
+    axi_write_32(32'h8000_0000 + S27KS_CFG0_REG_OFFSET, (s27ks_cfg0 | s27ks_cfg0 << 16));
+
+    // switch back to memory address space
+    reg_master.send_write(32'h7<<2, 1'b0, '1, s_reg_error);
+    if (s_axi_error != 1'b0) $error("unexpected error");
+
     $display("===========================");
     $display("= Random AXI transactions =");
     $display("===========================");
      
-    axi_master.run(TbNumReads, TbNumWrites);
+    axi_rand_mst.run(TbNumReads, TbNumWrites);
 
     $display("===========================");
     $display("=      Test finished      =");
@@ -179,13 +304,13 @@ module axi_hyper_tb
        reg_master.send_write(32'h20,1'b0,'1,s_reg_error);
        if (s_reg_error != 1'b0) $error("unexpected error");
 
-       axi_master.reset();
+       axi_rand_mst.reset();
 
        $display("===========================");
        $display("= Random AXI transactions =");
        $display("===========================");
 
-       axi_master.run(TbNumReads, TbNumWrites);
+       axi_rand_mst.run(TbNumReads, TbNumWrites);
 
        $display("===========================");
        $display("=      Test finished      =");
@@ -200,13 +325,13 @@ module axi_hyper_tb
        reg_master.send_write(32'h24,1'b1,'1,s_reg_error);
        if (s_reg_error != 1'b0) $error("unexpected error");
 
-       axi_master.reset();
+       axi_rand_mst.reset();
 
        $display("===========================");
        $display("= Random AXI transactions =");
        $display("===========================");
 
-       axi_master.run(TbNumReads, TbNumWrites);
+       axi_rand_mst.run(TbNumReads, TbNumWrites);
 
        $display("===========================");
        $display("=      Test finished      =");
@@ -225,7 +350,7 @@ module axi_hyper_tb
     .TbTestTime      ( TbTestTime         ),
     .AxiDataWidth    ( TbAxiDataWidthFull ),
     .AxiAddrWidth    ( TbAxiAddrWidthFull ),
-    .AxiIdWidth      ( TbAxiIdWidthFull   ),
+    .AxiIdWidth      ( TbAxiIdWidthFull+1 ),
     .AxiUserWidth    ( TbAxiUserWidthFull ),
 
     .RegAw           ( RegBusAW           ),
@@ -237,11 +362,11 @@ module axi_hyper_tb
     .axi_rule_t      ( rule_t             )
   ) i_dut_if (
     // clk and rst signal
-    .clk_i      ( clk             ),
-    .rst_ni     ( rst_n           ),
-    .end_sim_i  ( end_of_sim      ),
-    .axi_slv_if ( axi_mst_intf_dv ),
-    .reg_slv_if ( reg_bus_mst     )
+    .clk_i      ( clk          ),
+    .rst_ni     ( rst_n        ),
+    .end_sim_i  ( end_of_sim   ),
+    .axi_slv_if ( axi_dut_intf ),
+    .reg_slv_if ( reg_bus_mst  )
   );
 
 endmodule
