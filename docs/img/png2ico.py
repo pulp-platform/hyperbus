1871c7a0cd33d71a80f38edb6759ab623c7f9f0a
Andiruz <andiruz@gmail.com>
2026-06-24 12:46:15 +0200

    [DOC] removed no more used chips-IT images

--- docs/img/png2ico.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-img = Image.open("CHIPS_Symbolo_RGB_Colore.png")
-
-img.save("CHIPS_Symbolo_RGB_Colore.ico", format="ICO", sizes=[(16,16), (32,32), (48,48), (64,64), (128,128), (256,256)])
